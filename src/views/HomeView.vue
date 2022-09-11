709ac8af402a6077873db0f0e94ef615242c2ede
satokiy <syamada@netprotections.co.jp>
2022-09-11 19:16:46 +0900

    script setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <ScriptSetupTest title="this is title" @custom-event="parentMethod">
+    </ScriptSetupTest>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import ScriptSetupTest from "@/components/ScriptSetupTest.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
+    HelloWorld,
+    ScriptSetupTest,
+  },
+  methods: {
+    parentMethod(e){
+      console.log('exec emit')
+      console.log(e)
+    }
   }
-}
+};
 </script>
